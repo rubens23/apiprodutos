8b5d66ef71186187ae51a3511aa1924b1f624488
rubens23 <rubens_assis@outlook.com.br>
2022-07-05 10:43:28 -0300

    commit inicial
    api já ta feita com os metodos get, post, put e delete e agora vou tentar colocá-la no heroku

---
+++ apirest/src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -0,0 +1,17 @@
+package com.produtos.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class ApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
+
+}
+
+
